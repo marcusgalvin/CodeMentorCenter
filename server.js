e50020e7eed063e7216377fb4169ca10a4b205f5
m-tartag <mattbrs51@gmail.com>
2019-07-08 15:01:27 -0400

    added chalk like a nerd

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const chalk = require('chalk');
 const connectDB = require('./config/db');
 
 const app = express();
 
 // Connect DB
+
 connectDB();
 
 app.get('/test', (req, res) =>
   res.send('Hello World, this the Code Mentor Center backend server.')
 );
 
-// Init Built-In BodyParser
+// Init Built-In Parser
+
 app.use(express.json({ extended: false }));
 
 // Init API routes
@@ -20,4 +23,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
+app.listen(PORT, () =>
+  console.log(chalk.yellow.inverse(`Server is running on port:${PORT}`))
+);
