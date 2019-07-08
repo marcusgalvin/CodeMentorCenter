a62e685c8230493bcb6c8424660c6c90c9f07844
Steve B <stevebarriere.student@careerdevs.com>
2019-07-08 15:18:05 -0400

    Added page layout components.

---
+++ client/src/components/layout/index.js
@@ -0,0 +1,27 @@
+import ProfileCard from "./profileCard/ProfileCard";
+import ProfileDetails from "./profileCard/ProfileCard";
+import AboutUs from "./AboutUs";
+import CTARegister from "./CTARegister";
+import Footer from "./Footer";
+import HeroBanner from "./HeroBanner";
+import OnlineLearning from "./OnlineLearning";
+import RandomMentees from "./RandomMentees";
+import RandomMentors from "./RandomMentors";
+import Search from "./Search";
+import TopMentors from "./TopMentors";
+import TopNavbar from "./TopNavbar";
+
+export {
+  ProfileCard,
+  ProfileDetails,
+  AboutUs,
+  CTARegister,
+  Footer,
+  HeroBanner,
+  OnlineLearning,
+  RandomMentees,
+  RandomMentors,
+  Search,
+  TopMentors,
+  TopNavbar
+};
--- client/src/components/layout/profileCard/ProfileCard.jsx
+++ client/src/components/layout/profileCard/ProfileCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "./node_modules/react";
 import styles.css from ./
 
 export default class profilePage extends Component {
