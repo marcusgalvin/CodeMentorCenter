6cab8c7d74d71f07cb01df7526ecdf92a3c3dca8
Steve B <stevebarriere.student@careerdevs.com>
2019-07-10 09:08:36 -0400

    updated logo,banner and color scheme to reflect previous design.

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -5,52 +5,54 @@ import Search from "./Search";
 
 const Landing = () => {
   return (
-    <section className="landing">
-      <div className="landing-inner">
-        {/* CMC Hero Banner Container */}
-        <HeroBanner />
-        {/* Header Content w/ Register Button */}
-        <CTARegister />
-        {/* Search Field Container */}
-        <div className="search-landing">
-          <Search />
+    <div className="container">
+      <section className="landing">
+        <div className="landing-inner">
+          {/* CMC Hero Banner Container */}
+          <HeroBanner />
+          {/* Header Content w/ Register Button */}
+          <CTARegister />
+          {/* Search Field Container */}
+          <div className="search-landing">
+            <Search />
+          </div>
+          {/* Top Rated Mentors Showcase */}
+          <div className="top-rated-mentors">
+            <h4 className="lead">Top Rated Mentors</h4>
+            <div className="carousel-ct" />
+            Top Mentor's carousel container
+          </div>
+          {/* Higher Learning at CareerDevs */}
+          Online learning container
+          {/* Random Mentor Showcase */}
+          <div className="random-mentors">
+            <h4 className="lead">Mentors</h4>
+            <p>
+              Long established fact that a reader will be distracted by the
+              readable content of a page when looking at its layout. The point
+              of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using
+            </p>
+            <div className="carousel-ct" />
+            Random Mentor's carousel container
+          </div>
+          {/* Random Mentee Showcase */}
+          <div className="random-mentees">
+            <h4 className="lead">Mentees</h4>
+            <p>
+              Long established fact that a reader will be distracted by the
+              readable content of a page when looking at its layout. The point
+              of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using
+            </p>
+            <div className="carousel-ct" />
+            Random Mentee's carousel container
+          </div>
+          {/* Footer Container */}
+          footer component
         </div>
-        {/* Top Rated Mentors Showcase */}
-        <div className="top-rated-mentors">
-          <h4 className="lead">Top Rated Mentors</h4>
-          <div className="carousel-ct" />
-          Top Mentor's carousel container
-        </div>
-        {/* Higher Learning at CareerDevs */}
-        Online learning container
-        {/* Random Mentor Showcase */}
-        <div className="random-mentors">
-          <h4 className="lead">Mentors</h4>
-          <p>
-            Long established fact that a reader will be distracted by the
-            readable content of a page when looking at its layout. The point of
-            using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, as opposed to using
-          </p>
-          <div className="carousel-ct" />
-          Random Mentor's carousel container
-        </div>
-        {/* Random Mentee Showcase */}
-        <div className="random-mentees">
-          <h4 className="lead">Mentees</h4>
-          <p>
-            Long established fact that a reader will be distracted by the
-            readable content of a page when looking at its layout. The point of
-            using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, as opposed to using
-          </p>
-          <div className="carousel-ct" />
-          Random Mentee's carousel container
-        </div>
-        {/* Footer Container */}
-        footer component
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
