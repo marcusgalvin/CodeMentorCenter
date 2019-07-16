e426c02e01da6b3c4eeadcf31b2251730ad36459
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-07-16 14:04:47 -0400

    update aboutUs

--- client/src/components/layout/AboutUs.jsx
+++ client/src/components/layout/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import Footer from "./Footer";
 
 const AboutUs = () => {
   return (
@@ -16,28 +17,289 @@ const AboutUs = () => {
                 <div class="profileImg">
                   <i class="fas fa-user-circle" aria-hidden="true" />
                 </div>
-                <p>Marcus Galvin</p>
+                <p>Steve Barriere</p>
                 <br />
                 <p>Full Stack Web Developer</p>
+                <br />
+
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
                 </p>
               </div>
               <div className="flip-card-back">
-                <h1>Carlton Fisk</h1>
+                <h1>Steve Barriere</h1>
                 <p>Front-end Enthusiast</p>
                 <br />
                 <p>Specializes in</p>
                 <li>ReactJS</li>
                 <li>JavaScript</li>
-                <li>C++</li>
+                <li>Lorem Ipsum.....</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/steve_barriere">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* ############################## */}
+
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>Carl Kakisis</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>Carl Kakisis</h1>
+                <p>Back-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorum Ipsum...</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/CKakisis">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>Tom Blais</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>Tom Blais</h1>
+                <p>Front-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorem Ipsum...</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/tblais1224">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>Marcus Galvin</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>Marcus Galvin</h1>
+                <p>Front-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorem Ipsum...</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/MarcusGalvin">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div className="card-container">
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>JoseMiguel Marte</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>JoseMiguel Marte</h1>
+                <p>Front-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorem Ipsum...</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/musikit0">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>Hayden Bryant</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>Hayden Bryant</h1>
+                <p>Front-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorem Ipsum...</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/htbry20">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>Carl Durand</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>Carl Durand</h1>
+                <p>Front-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorem Ipsum</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/carlmarvyns">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div class="profileImg">
+                  <i class="fas fa-user-circle" aria-hidden="true" />
+                </div>
+                <p>Oscar Diahn</p>
+                <br />
+                <p>Full Stack Web Developer</p>
+                <br />
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+                </p>
+              </div>
+              <div className="flip-card-back">
+                <h1>Oscar Diahn</h1>
+                <p>Front-end Enthusiast</p>
+                <br />
+                <p>Specializes in</p>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>Lorem Ipsum...</li>
+                <br />
+                <div className="tweet">
+                  <a href="https://twitter.com/oddiahn1">
+                    <i class="fab fa-twitter-square" /> Twitter
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </Fragment>
   );
 };
