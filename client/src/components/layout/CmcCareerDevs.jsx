12b50ad680ba927d6f129e4b9e56bbaa6b230926
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-07-17 23:26:16 -0400

    updated CmcCareerDevs

--- client/src/components/layout/CmcCareerDevs.jsx
+++ client/src/components/layout/CmcCareerDevs.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
+import cmcNavLogo from "../images/logo_cmcNavbar.png";
 
 const OnlineLearning = () => {
   return (
@@ -14,97 +15,78 @@ const OnlineLearning = () => {
         </div>
         <br />
         <br />
-        <div className="CareerDev-photo">
-          <img src={require("../images/CareerDevs-Logo-600x200.jpg")} />
-        </div>
-        <br />
-        <div className="career-devs-info">
-          {/* <div className="left-info"> */}
-          <p>
-            <br />
-            CareerDevs is a year-long, full-stack program for students looking
-            to kickstart their careers in computer science. We are committed to
-            the belief that an education worth our students time is one that
-            teaches students the powerful skills needed to thrive in today’s
-            economy without incurring the crushing student debts affecting
-            Americans today. CareerDevs students graduate with an in-depth
-            understanding of and practical experience with a smorgasbord of the
-            most in-demand technologies in the job market today including C#,
-            Python, JavaScript, Java, C, SQL, object-oriented programming,
-            functional programming, Node.js, React, Redux, React Native, HTML
-            and CSS. Beyond these specific technologies, CareerDevs also trains
-            students to be resourceful problem-solvers by approaching problems
-            as computer scientists. We don’t just train you what you need to
-            know; we train you how to think, setting you up for a lifetime of
-            success beyond the CareerDevs classroom.
-          </p>
-          {/* </div> */}
-          {/* <div className="right-info">
-            <p>
-              <br />
-              The Best Coding and Web Development Opportunity on the Planet If
-              You Are Hired Into Our Paid Internship After Six Months, We Cancel
-              Your Remaining Balance (Paid Internship Earning Potential: $1,000
-              – $2,000 per month – never to be repaid. It’s yours!) Guaranteed
-              Full-Time Job Placement After Graduation, Earning $50,000 –
-              $90,000
-            </p>
-          </div> */}
-        </div>
-        <br />
-        <br />
-        <div className="students">
-          <h1>
-            Here Are Just A Few of Our Employed Grads! Each graduate began a new
-            career earning between $50,000 to $90,000! If you don’t do the same
-            within 5 months after graduating you don’t pay us anything.
-          </h1>
-          <div className="student-photos">
-            <img src={require("../images/EmitonAlves.png")} />
-            <p>Emiton Alves Software Engineer</p>
-            <img src={require("../images/RebeccaWagaman.jpg")} />
+
+        <div className="photo-ct">
+          <div className="CMC-photo">
+            <img src={cmcNavLogo} alt="Code Mentor Center" />
+          </div>
+
+          <div className="CareerDev-photo">
+            <img src={require("../images/CareerDevs-Logo-600x200.jpg")} />
           </div>
         </div>
-        <br />
-        <br />
 
-        <div className="what-you-learn">
-          <p>
-            <h2>What You Will Learn</h2>
-            <li>
-              Introduction to website development and programming (4 months): We
-              start from the very beginning. We take it slowly, allowing you to
-              learn without being overwhelmed. In addition to being able to ask
-              your instructors questions, every CareerDevs student also receives
-              a mentor (a student from Intermediate or Advanced), whom you can
-              ask questions. By the end of the Introduction phase, you’ve built
-              your first full-stack application. Also, your friends now think
-              you’re weird. 🙂
-            </li>{" "}
-            <li>
-              Intermediate website development and programming (4 months). After
-              completing the Introduction phase, you will have built up your
-              confidence. You are now ready to learn more full-stack computer
-              science concepts and build more robust websites and software
-              programs. You are also eating algorithms for breakfast, building
-              API’s, mastering recursion and can consider yourself an
-              object-oriented developer. Most Impressive!
-            </li>{" "}
-            <li>
-              dvanced website development and programming (4 months). Yes, you
-              are here now. You are now learning to master the most advanced,
-              cutting edge & real-world software development concepts. You have
-              developed several full-stack milestone software projects and a
-              capstone project that would impress Bill Gates. Oh, by the way…
-              you are also earning $50,000 to $80,000 per year. No joke. It’s
-              ok. Tell your friends and family… you’re not bragging, you worked
-              hard for this.
-            </li>
-            A
-          </p>
+        <div className="middle-info">
+          <div className="left-info">
+            <p>
+              Code Mentor Centor is your gateway into a successful career in
+              computer science & programming. After spending time learning the
+              basic fundamentals of JavaScript with your mentor, you are able to
+              continue your education at Career Devs Computer Science
+              University. Career Devs is the Best Coding and Web Development
+              Opportunity on the Planet! If You Are Hired Into Our Paid
+              Internship After Six Months, We Cancel Your Remaining Balance. Its
+              Yours!
+            </p>
+          </div>
+          <div className="right-info">
+            <p>
+              CareerDevs is a year-long, full-stack program for students looking
+              to kickstart their careers in computer science. We are committed
+              to the belief that an education worth our students time is one
+              that teaches students the powerful skills needed to thrive in
+              today’s economy without incurring the crushing student debts
+              affecting Americans today. Guaranteed Full-Time Job Placement
+              After Graduation, Earning $50,000 – $90,000
+            </p>
+          </div>
         </div>
-        <Footer />
       </div>
+      <br />
+      <br />
+      <div className="students">
+        <h2>
+          Here Are Just A Few of Our Employed Grads! Each graduate began a new
+          career earning between $50,000 to $90,000! If you don’t do the same
+          within 5 months after graduating you don’t pay us anything.
+        </h2>
+        <br />
+        <div className="student-photos-ct">
+          <div className="single-student">
+            <img src={require("../images/EmitonAlves.png")} />
+            <p>Emiton Alves</p>
+            <p>Software Engineer</p>
+          </div>
+          <div className="single-student">
+            <img src={require("../images/RebeccaWagaman.jpg")} />
+            <p>Rebecca Wagaman</p>
+            <p>Software Engineer</p>
+          </div>
+          <div className="single-student">
+            <img src={require("../images/MichaelDAntuono.jpg")} />
+            <p>Michael DAntuono</p>
+            <p>Software Engineer</p>
+          </div>
+          <div className="single-student">
+            <img src={require("../images/JamalDawson.jpg")} />
+            <p>Jamal Dawson</p>
+            <p>Software Engineer</p>
+          </div>
+        </div>
+      </div>{" "}
+      <br />
+      }
+      <Footer />
     </Fragment>
   );
 };
