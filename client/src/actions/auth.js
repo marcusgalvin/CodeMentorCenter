9797aac53de40643e094b6a7943bb236212e7bbf
tblais1224 <thomasblais.student@careerdevs.com>
2019-07-18 23:29:20 -0400

    auth actions / reducers look good

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -10,8 +10,98 @@ import {
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
-
 //load logged in user
 export const loadUser = () => async dispatch => {
-    
-}
+  if (localStorage.token) {
+    //set token to global header
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
+
+//register user
+export const register = ({
+  name,
+  email,
+  password,
+  password2
+}) => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  const body = JSON.stringify({
+    name,
+    email,
+    password,
+    password2
+  });
+  try {
+    const res = await axios.post("/api/users", body, config);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+    dispatch(loadUser());
+  } catch (error) {
+    //will be an array of errors
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach(element => {
+        //set a danger alert box with the error message
+        dispatch(setAlert(element.msg, "danger"));
+      });
+    }
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
+};
+
+//login user
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  const body = JSON.stringify({
+    email,
+    password
+  });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+    dispatch(loadUser());
+  } catch (error) {
+    //will be an array of errors
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach(element => {
+        //set a danger alert box with the error message
+        dispatch(setAlert(element.msg, "danger"));
+      });
+    }
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
